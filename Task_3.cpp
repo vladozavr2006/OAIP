f86aebe89e4a3b852788c455af61708e24198c40
vladozavr <vladozavr2006@mail.ru>
2024-09-22 22:54:04 +0300

    Вроде работает

--- Task_3.cpp
+++ Task_3.cpp
@@ -1,16 +1,16 @@
-void main() {
-Double touble, start, end, step;
+#include <iostream>
+#include <cmath>
+int main() {
+double start, end, step;
 std::cout << "Введите начальное значение x: ";
 std::cin >> start; // Задаем исходное значение переменной
 std::cout << "Введите конечное значение x: ";
 std::cin >> end; // Задаем конечно значение переменной
 std::cout << "Введите шаг: ";
-std::cin >> step; // Задаем шаг, с которым с которым будет изменяться
-аргумент
+std::cin >> step; // Задаем шаг, с которым с которым будет изменяться аргумент
 for (double x = start; x <= end; x += step) {
 double y = sin(x); // функция y = sin(x)
-std::cout << x << " | " << y << std::endl; // Выводим значения
-функции для каждого аргумента (x | y)
+std::cout << x << " | " << y << std::endl; // Выводим значения функции для каждого аргумента (x | y)
 }
-return nullptr;
+return 0;
 }
